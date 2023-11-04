5cbe7cee3667c0eb6a472fc9a31137643076c0c9
tango3304 <94199810+tango3304@users.noreply.github.com>
2023-11-05 06:17:24 +0900

    Update tping.py
    remove [Generate 32pieces RandomString]

--- tping/tping.py
+++ tping/tping.py
@@ -1,28 +1,27 @@
 from importlib import import_module
-from string import ascii_letters
-from random import choices, randint
+from random import randint
 from tchecksum.tchecksum import CheckSum
 from datetime import datetime
+from sys import exit, exc_info
+from traceback import print_tb, format_exception_only
 socket = import_module('socket')
 
 
 class PingSocket:
-
 	def __init__(self, ipaddress, timeout):
 	# Check ipaddress and data StrType [ipaddressとdataが文字列型か確認]
 		if (type(ipaddress) is not str):
 			print(f'No Arguments IPaddress StrType [IPaddress引数が文字列型ではない]')
-			return exit(1)
+			return exit
 		if type(timeout) != int:
 			print(f'No Arguments Timeout IntType [Timeout引数が整数型ではない]')
-			return exit(1)
+			return exit
 		
 		self.ipaddr = ipaddress
 		self.timeout = timeout
 
 	def ping_socket(self):
 	# [初期設定]
-		data = None
 		bufsize = 4096
 		receive_result = None
 		send_timestamp = None
@@ -31,8 +30,7 @@ class PingSocket:
 		try:
 		# Add Generate data [作成したdataを追加]
 		# Create IMCP Packet [ICMPパケット作成]
-			data = PingSocket.generate_ramdom_data()
-			icmp_packet = PingSocket.send_icmp_packet(data)
+			icmp_packet = PingSocket.send_icmp_packet()
 
 		# Destination Send ICMP Packet [宛先にICMPパケットを送信]
 		# Destination Connection SendPacket And Return RecivePacket [接続できれば、パケットを送信し、受信パケットを返す]
@@ -45,6 +43,7 @@ class PingSocket:
 				receive_timestamp = datetime.now() - send_timestamp
 			
 			# Send and Receive Time[送受信の時間]
+			# Truncate DecimalPoint 4Below [小数点4以下を切り捨て]
 				send_receive_timestamp = float(str(receive_timestamp).replace(':', ''))
 				send_receive_timestamp = round(send_receive_timestamp, 3)
 				return receive_result ,send_receive_timestamp
@@ -53,18 +52,20 @@ class PingSocket:
 			receive_result = 'Timeout'
 			send_receive_timestamp = None
 			return receive_result , send_receive_timestamp
-	
-	
-	def generate_ramdom_data():
-	# Generate data Length [dataの長さを生成]
-	# Generate data Length Values [dataの長さ分の値を生成]
-	# Return Value string Type [戻り値 文字列型]
-		random_int = 32
-		random_values = ''.join(choices(ascii_letters, k=random_int))
-		return random_values
+		except KeyboardInterrupt:
+			print(f'\n\nProcess Interrupted [処理を中断しました]')
+			exit()
+		except:
+			# Get ErrorMessage [エラーメッセージ取得]
+				exc_type, exc_message, exc_object = exc_info()
+				print_tb(exc_object)
+				exc_list = format_exception_only(exc_type, exc_message)
+				error_message = ''.join(exc_message for exc_message in exc_list)
+				print(f'  {error_message}')
+				exit()
 	
 
-	def send_icmp_packet(data):
+	def send_icmp_packet():
 	# Initialization Variables [変数の初期化]
 		"""
 		Type: 08
@@ -85,12 +86,11 @@ class PingSocket:
 	# Calculation Checksum [チェックサムを計算]
 	# Split for Packet ChecksumValue [チェックサム値をパケット用に分割]
 	# tchecksum: https://github.com/tango3304/tchecksum/blob/main/tchecksum/tchecksum.py
-		checksum = CheckSum(icmp_type, icmp_code, icmp_id1, icmp_id2, icmp_seq1, icmp_seq2, data).t_checksum()
+		checksum, data_unicode = CheckSum(icmp_type, icmp_code, icmp_id1, icmp_id2, icmp_seq1, icmp_seq2).t_checksum()
 		checksum1 = checksum >> 8
 		checksum2 = ((checksum << 8) & 0xffff) >> 8
 
-	# AddList type & code & checksum & id & seq & data [type & code & checksum & id & seq & dataをリスト追加]
+	# Generate SendList [送信用リストの生成]
 	# Return Value bytes Type [戻り値 bytes]
-		send_packet = [icmp_type, icmp_code, checksum1, checksum2, icmp_id1, icmp_id2, icmp_seq1, icmp_seq2]
-		send_packet.extend(data.encode('UTF-8'))
+		send_packet = [icmp_type, icmp_code, checksum1, checksum2, icmp_id1, icmp_id2, icmp_seq1, icmp_seq2] + data_unicode
 		return bytes(send_packet)
